a23254c920455d99be3702ec78b9493884c61dae
miodraggosic <gosmio88@gmail.com>
2020-08-06 23:59:50 +0200

    15. cas test nizovi

--- 14. cas/Nizovi/nizovi.js
+++ 14. cas/Nizovi/nizovi.js
@@ -15,7 +15,7 @@ for (i = 0; i < cars.length; i++) {
 
 //zadatak 2 
 
-let niz = [47, 12, 45, 42, 6];
+let niz = [-47, 12, -45, 42, 66];
 let suma = 0;
 for (i = 0; i < niz.length; i++) {
     suma += niz[i];
@@ -78,11 +78,147 @@ console.log(min(niz));
 
 //zadatak 7
 
-let index = niz.indexOf (max(niz));
-console.log (index);
+// let index = niz.indexOf (max(niz));
+// console.log (index);
+
+// drugi nacin
+let maxIndex = nekiNiz => {
+   let broj = 0;
+   let maks = nekiNiz[0];
+    for (i = 0; i < nekiNiz.length; i++) {
+        if (maks < nekiNiz[i]) {
+            maks = nekiNiz[i];
+            broj = i;
+            
+        } 
+    } return broj;
+    
+}
+
+let indexi = maxIndex(niz);
+console.log (indexi);
+
+
+
 
 
 //zadatak 8
-let ind = niz.indexOf (min(niz));
+// let ind = niz.indexOf (min(niz));
 
-console.log (ind);
+// console.log (ind);
+
+
+
+
+// zadatak 9 
+
+let veciOdSred = nekiNiz => {
+    let s = sred(nekiNiz);
+    console.log (s);
+    let broj = 0;
+
+    for (i = 0; i < nekiNiz.length; i++) {
+        if (nekiNiz[i] > s ) {
+            broj++;
+        }
+    } return broj;
+}
+
+console.log (veciOdSred(niz));
+
+
+// zadatak 10 
+
+let pozitiv = nekiNiz => {
+    let zbir = 0;
+    for (i = 0; i < nekiNiz.length; i++) {
+        if (nekiNiz[i] >= 0) {
+            zbir += nekiNiz[i];
+        }
+    } return zbir;
+}
+
+console.log (pozitiv(niz));
+
+// zadatak 11
+
+let parnih = nekiNiz => {
+    let broj = 0;
+    for (i = 0; i < nekiNiz.length; i++) {
+        if (nekiNiz[i] % 2 == 0) {
+            broj++;
+        }
+    } return broj; 
+}
+
+console.log (parnih(niz));
+
+
+//zadatak 12
+
+
+let parNepar = nekiNiz => {
+    let broj = 0;
+    for (i = 0; i < nekiNiz.length; i++) {
+        if (nekiNiz[i] % 2 == 0 && i % 2 != 0) {
+            broj++;
+        }
+    } return broj; 
+}
+
+console.log (parNepar(niz));
+
+
+//zadatak 13
+let sumaPar = nekiNiz => {
+    let suma = 0;
+    for (i = 0; i < nekiNiz.length; i++) {
+        if (i % 2 == 0) {
+            suma += nekiNiz[i];
+        }
+    } return suma; 
+}
+
+console.log (sumaPar(niz));
+
+
+// zadatak 14
+let menjamZnak = nekiNiz => {
+    for (i = 0; i < nekiNiz.length; i++) {
+        nekiNiz[i] = nekiNiz[i] * -1;
+        
+        
+    } return nekiNiz;; 
+}
+
+console.log (menjamZnak(niz));
+
+//zadatak 15
+
+let menjamNeparPar = nekiNiz => {
+    for (i = 0; i < nekiNiz.length; i++) {
+        if (nekiNiz[i] % 2 != 0 && i % 2 == 0) {
+            nekiNiz[i] = nekiNiz[i] * -1;
+        }
+    } return nekiNiz; 
+}
+
+console.log (menjamNeparPar(niz));
+
+//zadatak 16
+
+let shop = ["secer", "brasno", "voda", "mleko"];
+let divShop = document.getElementById("shop");
+let kupovina = nekiNiz => {
+    for (i = 0; i < shop.length; i++) {
+        divShop.innerHTML += `<ul><li>${shop[i]}<li></ul>`;
+    }
+
+}
+
+kupovina(shop);
+
+
+//zadatak 17
+
+
